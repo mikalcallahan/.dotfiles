940d3c990e57c4d9ed3bfaea4cc0a45780288e92
Mikal Callahan <mikal.callahan@icloud.com>
2023-03-03 21:11:22 -0500

    vpn indicator by wifi color

--- sketchybar/items/wifi.sh
+++ sketchybar/items/wifi.sh
@@ -1,4 +1,5 @@
 sketchybar --add item wifi right                         \
            --set wifi    script="$PLUGIN_DIR/wifi.sh"    \
                          background.padding_right=10     \
-                         update_freq=5
+                         update_freq=15                  \
+           --subscribe wifi_change                       \
--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env sh
 
+source "$HOME/.config/sketchybar/colors.sh"
+
 CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
 SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
 CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
+VPN_STATUS=$(echo ivpn status | grep "\bCONNECTED\b" | wc -l)
 
 if [ "$SSID" = "" ]; then
   sketchybar --set $NAME label="Disconnected" icon=睊
 else
-  # sketchybar --set $NAME label="$SSID (${CURR_TX}Mbps)" icon=直
-  sketchybar --set $NAME label="$SSID" icon=
+  if [[ $STATUS != 0 ]]; then
+    sketchybar --set $NAME label="$SSID" icon= icon.color=$GREEN
+    # sketchybar --set $NAME label="$SSID (${CURR_TX}Mbps)" icon=直
+  else
+    sketchybar --set $NAME label="$SSID" icon= icon.color=$ORANGE
+  fi
 fi
