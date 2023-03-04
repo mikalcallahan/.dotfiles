212de7fc908f9aa42b74ea2d7f5f296e2bcbe175
Mikal Callahan <mikal.callahan@icloud.com>
2023-03-04 11:23:37 -0500

    Improvement on vpn connected logic

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -5,13 +5,13 @@ source "$HOME/.config/sketchybar/colors.sh"
 CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
 SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
 CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
-VPN_STATUS=$(echo ivpn status | grep "\bCONNECTED\b" | wc -l)
+VPN_STATUS="$(ivpn status | grep "\bCONNECTED\b")"
 
 if [ "$SSID" = "" ]; then
   sketchybar --set $NAME label="Disconnected" icon=睊
 else
-  if [[ $STATUS != 0 ]]; then
-    sketchybar --set $NAME label="$SSID" icon= icon.color=$GREEN
+  if [[ $VPN_STATUS  =~ "CONNECTED" ]]; then
+    sketchybar --set $NAME label="$SSID" icon= icon.color=$WHITE
     # sketchybar --set $NAME label="$SSID (${CURR_TX}Mbps)" icon=直
   else
     sketchybar --set $NAME label="$SSID" icon= icon.color=$ORANGE
