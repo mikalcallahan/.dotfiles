c56bfdca8ad05e91fb8acfa5eb9f65c3921fd8ba
Mikal Callahan <mikal.callahan@icloud.com>
2023-07-22 08:30:32 -0400

    typescript debugger

---
+++ nvim/lua/plugins/mason-nvim-dap.lua
@@ -0,0 +1,7 @@
+return {
+  "jay-babu/mason-nvim-dap.nvim",
+  opts = {
+    -- You can provide additional configuration to the handlers,
+    -- see mason-nvim-dap README for more information
+  },
+}
