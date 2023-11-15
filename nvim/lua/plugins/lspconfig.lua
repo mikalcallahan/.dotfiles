6de421e2daa3e9df2ecec2ef8e17fdc7362ac1f1
Mikal Callahan <mikal.callahan@icloud.com>
2023-11-15 16:31:25 -0500

    main - chore(nvim#lspconfig.lua): set virtual_text = false and underline = true

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,10 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    diagnostics = {
+      underline = true,
+      virtual_text = false,
+    },
     servers = {
       -- pyright will be automatically installed with mason and loaded with lspconfig
       sourcekit = {
