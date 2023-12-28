f63a7585fbc2ece7463a2d60ef73ef652b989a5e
Mikal Callahan <mikal@dovera.local>
2023-12-28 16:31:32 -0500

    main - feat(sketchybar#calendar): click script to open notification center

--- sketchybar/items/calendar.sh
+++ sketchybar/items/calendar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-#CLICK_SCRIPT=$(osascript -e 'tell application "System Events" to key code 111')
+CLICK_SCRIPT="osascript -e 'tell application \"System Events\" to key code 111'"
 
 sketchybar --add item calendar right \
 	--set calendar icon.font="$FONT:0xff$CFORE:12.0" \
@@ -9,7 +9,7 @@ sketchybar --add item calendar right \
 	label.align=right \
 	background.padding_left=10 \
 	update_freq=15 \
+	click_script="$CLICK_SCRIPT" \
 	script="$PLUGIN_DIR/calendar.sh" \
 	\
-	--subscribe calendar system_woke # click_script="$CLICK_SCRIPT" \
-# click_script="$PLUGIN_DIR/zen.sh" # click_script="$CLICK_SCRIPT" \
+	--subscribe calendar system_woke
