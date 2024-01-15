f277e665f6005e7139b19008206e0954246c119d
Mikal Callahan <mikal@dovera.local>
2024-01-15 08:22:40 -0500

    main - feat(sketchybar): add logout to apple script menu

--- sketchybar/items/apple.sh
+++ sketchybar/items/apple.sh
@@ -11,23 +11,13 @@ sketchybar --add item apple.logo left \
 	popup.height="50" \
 	click_script="$POPUP_CLICK_SCRIPT" \
 	\
-	--add item apple.prefs popup.apple.logo \
-	--set apple.prefs icon=$PREFERENCES \
-	label="Preferences" \
-	label.font="$FONT:Regular:13" \
-	background.padding_right=25 \
-	background.padding_left=25 \
-	y_offset="-5" \
-	click_script="open -a 'System Preferences';
-                                              $POPUP_OFF" \
-	\
 	--add item apple.activity popup.apple.logo \
 	--set apple.activity icon=$ACTIVITY \
 	label="Activity" \
 	label.font="$FONT:Regular:13" \
 	background.padding_right=25 \
 	background.padding_left=25 \
-	y_offset="5" \
+	y_offset="-5" \
 	click_script="open -a 'Activity Monitor';
                                               $POPUP_OFF" \
 	\
@@ -37,6 +27,34 @@ sketchybar --add item apple.logo left \
 	label.font="$FONT:Regular:13" \
 	background.padding_right=25 \
 	background.padding_left=25 \
-	y_offset="15" \
+	y_offset="0" \
 	click_script="pmset displaysleepnow;
+                                              $POPUP_OFF" \
+	--add item apple.prefs popup.apple.logo \
+	--set apple.prefs icon=$PREFERENCES \
+	label="Preferences" \
+	label.font="$FONT:Regular:13" \
+	background.padding_right=25 \
+	background.padding_left=25 \
+	y_offset="5" \
+	click_script="open -a 'System Preferences';
+                                              $POPUP_OFF" \
+	\
+	--add item apple.restart popup.apple.logo \
+	--set apple.restart icon=$RESTART \
+	label="Restart" \
+	label.font="$FONT:Regular:13" \
+	background.padding_right=25 \
+	background.padding_left=25 \
+	y_offset="8" \
+	click_script="osascript -e 'tell app \"loginwindow\" to «event aevtrrst»';
+                                              $POPUP_OFF" \
+	--add item apple.shutdown popup.apple.logo \
+	--set apple.shutdown icon=$SHUTDOWN \
+	label="Shutdown" \
+	label.font="$FONT:Regular:13" \
+	background.padding_right=80 \
+	background.padding_left=25 \
+	y_offset="10" \
+	click_script="osascript -e 'tell app \"loginwindow\" to «event aevtrsdn»';
                                               $POPUP_OFF"
--- sketchybar/plugins/brew.sh
+++ sketchybar/plugins/brew.sh
@@ -3,22 +3,25 @@
 source "$HOME/.config/sketchybar/colors.sh"
 source "$HOME/.config/sketchybar/icons.sh"
 
-
 COUNT=$(brew outdated | wc -l | tr -d ' ')
 
 COLOR=$RED
 
 case "$COUNT" in
-  [3-5][0-9]) sketchybar --set $NAME label=$COUNT icon.color=$ORANGE icon=$PACKAGE
-  ;;
-  [1-2][0-9]) sketchybar --set $NAME label=$COUNT icon.color=$YELLOW icon=$PACKAGE
-  ;;
-  [1-9]) sketchybar --set $NAME label=$COUNT icon.color=$WHITE icon=$PACKAGE
-  ;;
-  0) COLOR=
-     COUNT=
-     sketchybar --set $NAME label= icon.color= icon=
-  ;;
+[3-5][0-9])
+	sketchybar --set $NAME label=$COUNT icon.color=$ORANGE icon=$PACKAGE
+	;;
+[1-2][0-9])
+	sketchybar --set $NAME label=$COUNT icon.color=$CBLUE icon=$PACKAGE
+	;;
+[1-9])
+	sketchybar --set $NAME label=$COUNT icon.color=$CBLUE icon=$PACKAGE
+	;;
+0)
+	COLOR=$RED
+	COUNT=
+	sketchybar --set $NAME label= icon.color=$ICON_COLOR icon=$PACKAGE
+	;;
 esac
 
 # sketchybar --set $NAME label=$COUNT icon.color=$COLOR icon=$PACKAGE
