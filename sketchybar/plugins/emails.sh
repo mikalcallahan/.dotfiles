bf58aca1144301f225dc5719e600ffc7e6c09eb7
Mikal Callahan <mikal@dovera.local>
2024-01-21 13:20:33 -0500

    main - refactor(sketchybar): hide # of unread emails

--- sketchybar/plugins/emails.sh
+++ sketchybar/plugins/emails.sh
@@ -4,16 +4,16 @@ source "$HOME/.config/sketchybar/colors.sh"
 source "$HOME/.config/sketchybar/icons.sh"
 
 ICON="$EMAIL"
-LABEL="$(osascript $HOME/.config/sketchybar/scripts/emails.applescript)"
-if [[ $LABEL == "" ]]; then
+EMAILS="$(osascript $HOME/.config/sketchybar/scripts/emails.applescript)"
+if [[ $EMAILS == "" ]]; then
 	ICON=
 	COLOR="$CMAGENTA"
-elif [[ "$LABEL" -gt 10 && "$LABEL" -le 25 ]]; then
+elif [[ "$EMAILS" -gt 10 && "$EMAILS" -le 25 ]]; then
 	COLOR="$CMAGENTA"
-elif [[ "$LABEL" -gt 25 ]]; then
+elif [[ "$EMAILS" -gt 25 ]]; then
 	COLOR="$CBLUE"
 else
 	COLOR="$ICON_COLOR"
 fi
 
-sketchybar --set $NAME icon=$ICON label="${LABEL}" icon.color=$COLOR
+sketchybar --set $NAME icon=$ICON label="" icon.color=$COLOR
