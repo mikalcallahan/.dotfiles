a01393e8418a42a17daf51e783c4e330a07e3536
Mikal Callahan <mikal.callahan@icloud.com>
2024-07-31 13:31:02 -0400

    main - feat(nvim(nvim-unception)): open_buffer_in_new_tab = false

--- nvim/lua/plugins/nvim-unception.lua
+++ nvim/lua/plugins/nvim-unception.lua
@@ -2,6 +2,6 @@ return {
   "samjwill/nvim-unception",
   init = function()
     -- Optional settings go here!
-    vim.g.unception_open_buffer_in_new_tab = true
+    vim.g.unception_open_buffer_in_new_tab = false
   end,
 }
