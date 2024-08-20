db0a8845db322185d1611c0aa27cff3b41e6a441
Mikal Callahan <mikal.callahan@icloud.com>
2024-08-20 09:09:57 -0400

    delovni - chore(nvim(chatgpt)): use gpg to get api_key_cmd

--- nvim/lua/plugins/chatgpt.lua
+++ nvim/lua/plugins/chatgpt.lua
@@ -3,8 +3,9 @@ return {
   event = "VeryLazy",
   enabled = false,
   config = function()
+    local home = vim.fn.expand("$HOME")
     require("chatgpt").setup({
-      api_key_cmd = "bw get password openai-key",
+      api_key_cmd = "gpg --decrypt " .. home .. "/.config/.apikey.txt.gpg",
       openai_params = {
         model = "gpt-4o",
       },
