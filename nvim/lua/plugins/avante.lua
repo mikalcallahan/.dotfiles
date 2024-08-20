67cbb46abc0995a93fa3069859e17987b6f17db6
Mikal Callahan <mikal.callahan@icloud.com>
2024-08-20 08:25:01 -0400

    main - feat(nvim): avante & chatGPT fixes

---
+++ nvim/lua/plugins/avante.lua
@@ -0,0 +1,28 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  build = "make",
+  opts = {
+    -- add any opts here
+    provider = "openai",
+  },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    {
+      "grapp-dev/nui-components.nvim",
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+      },
+    },
+    --- The below is optional, make sure to setup it properly if you have lazy=true
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
--- nvim/lua/plugins/chatgpt.lua
+++ nvim/lua/plugins/chatgpt.lua
@@ -5,7 +5,14 @@ return {
     require("chatgpt").setup({
       api_key_cmd = "bw get password openai-key",
       openai_params = {
-        model = "gpt-4o",
+        model = "gpt-4o-2024-08-06",
+        temperature = 1,
+        top_p = 1,
+      },
+      chat = {
+        keymaps = {
+          close = "Qq",
+        },
       },
     })
   end,
