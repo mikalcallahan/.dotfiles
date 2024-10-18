9061d8133f271c0014eec75c92f5f6174d00fa77
Mikal Callahan <mikal.callahan@icloud.com>
2024-10-18 10:20:58 -0400

    main - fix(nvim(avante)): config

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -10,21 +10,32 @@ return {
     },
   },
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
+    "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
+      -- support for image pasting
       "HakonHarnes/img-clip.nvim",
       event = "VeryLazy",
-    },
-    {
-      "grapp-dev/nui-components.nvim",
-      dependencies = {
-        "MunifTanjim/nui.nvim",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
       },
     },
-    --- The below is optional, make sure to setup it properly if you have lazy=true
     {
+      -- Make sure to set this up properly if you have lazy=true
       "MeanderingProgrammer/render-markdown.nvim",
       opts = {
         file_types = { "markdown", "Avante" },
