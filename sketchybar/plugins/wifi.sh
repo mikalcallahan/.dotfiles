f3906ad6d10e23fa146ff86f606109a2e23a1356
Mikal Callahan <mikal.callahan@icloud.com>
2024-10-22 18:08:33 -0400

    fix(sketchybar): ipaddress for macos 15+

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -2,48 +2,30 @@
 
 source "$HOME/.config/sketchybar/colors.sh"
 
-# CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
-CURRENT_WIFI="$(networksetup -getairportnetwork en0)"
-SSID="$(echo "$CURRENT_WIFI" | grep -o ": .*" | sed 's/^: //')"
-# CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
-#VPN_STATUS="$(echo 'ivpn status | grep "\bCONNECTED\b"')"
-VPN_STATUS=$(/usr/local/bin/ivpn status)
-VPN_CONNECTED=$(echo $VPN_STATUS | grep '\bCONNECTED\b')
-VPN_PAUSED=$(echo $VPN_STATUS | grep '\bPAUSED\b')
-# VPN_CONNECTED="$(ivpn status | grep "\bCONNECTED\b")"
-# VPN_PAUSED="$(ivpn status | grep "\bPAUSED\b")"
-# IS_VPN=$("scutil --nwi | grep -m1 'utun' | awk '{ print $1 }'")
-# VPN_STATUE=false
+OS_VERSION="$(sw_vers | grep 'ProductVersion' | awk -F'[ .:]+' '{print $2}' | xargs)"
 
-if [ "$SSID" = "" ]; then
-	sketchybar --set $NAME label="Disconnected" icon=睊
+if [ "$OS_VERSION" -gt "14" ]; then
+  IP_ADDRESS="$(networksetup -getinfo "Wi-Fi" | grep "IP address" | awk -F ': ' '{print $2}' | awk 'NR==1')"
+  SSID="$IP_ADDRESS"
 else
-	if [[ $VPN_CONNECTED =~ "CONNECTED" ]]; then
-		sketchybar --set $NAME label="$SSID" icon= icon.color=$ICON_COLOR
-	elif [[ $VPN_PAUSED =~ "PAUSED" ]]; then
-		sketchybar --set $NAME label="$SSID" icon= icon.color=$ICON_COLOR_WARNING
-	else
-		sketchybar --set $NAME label="$SSID" icon= icon.color=$ICON_COLOR_ALERT
-	fi
+  CURRENT_WIFI="$(networksetup -getairportnetwork en0)"
+  # CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
+  SSID="$(echo "$CURRENT_WIFI" | grep -o ": .*" | sed 's/^: //')"
+  SSID="FAIL"
 fi
 
-# IP_ADDRESS=$(scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1)
-# IS_VPN=$(scutil --nwi | grep -m1 'utun' | awk '{ print $1 }')
-#
-# if [[ $IS_VPN != "" ]]; then
-# 	COLOR=$CYAN
-# 	ICON=
-# 	LABEL="VPN"
-# elif [[ $IP_ADDRESS != "" ]]; then
-# 	COLOR=$BLUE
-# 	ICON=
-# 	LABEL=$IP_ADDRESS
-# else
-# 	COLOR=$WHITE
-# 	ICON=
-# 	LABEL="Not Connected"
-# fi
-#
-# sketchybar --set $NAME \
-# 	icon=$ICON \
-# 	label="$LABEL"
+VPN_STATUS=$(/usr/local/bin/ivpn status)
+VPN_CONNECTED=$(echo "$VPN_STATUS" | grep '\bCONNECTED\b')
+VPN_PAUSED=$(echo "$VPN_STATUS" | grep '\bPAUSED\b')
+
+if [ "$SSID" = "" ]; then
+  sketchybar --set "$NAME" label="Disconnected" icon=睊
+else
+  if [[ $VPN_CONNECTED =~ "CONNECTED" ]]; then
+    sketchybar --set "$NAME" label="$SSID" icon= icon.color="$ICON_COLOR"
+  elif [[ $VPN_PAUSED =~ "PAUSED" ]]; then
+    sketchybar --set "$NAME" label="$SSID" icon= icon.color="$ICON_COLOR_WARNING"
+  else
+    sketchybar --set "$NAME" label="$SSID" icon= icon.color="$ICON_COLOR_ALERT"
+  fi
+fi
