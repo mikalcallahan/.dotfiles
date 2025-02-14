9f542476c6c0bed4ec34e0dc78d8c8c2ecac0aa4
Mikal Callahan <mikal.callahan@icloud.com>
2025-02-13 19:12:24 -0800

    main - feat(nvim): blink-cmp

---
+++ nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,10 @@
+return {
+  "Saghen/blink.cmp",
+  opts = {
+    eymap = {
+      preset = "enter",
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+    },
+  },
+}
